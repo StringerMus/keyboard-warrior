b32486a02a5eb2f5f0655ac809ef13b16e91c16f
Musaddek Ali <mussali.one@gmail.com>
2024-01-01 15:26:41 +0000

    js add commands to shuffle challenges

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,25 +1,16 @@
 //challenge sentences
-let sentence = ["once in a blue moon","a piece of cake","easier said than done","actions speak louder than words","beat around the bush","the ball is in your court"]
+let sentence = [
+    "love you to the moon and back","Don't count your chickens before they hatch","A bird in the hand is worth two in the bush","Slow and steady wins the race","stop beating around the bush","the ball is in your court","You can't judge a book by its cover"
+];
 
 //randomise sentences
-let shuffle = Math.floor(Math.random() * sentence.length);
-console.log(shuffle, sentence[shuffle]);
+let shuffle = sentence[Math.floor(Math.random() * sentence.length)];
 
+let challengeWord = document.getElementById('challenge-word');
+challengeWord.innerHTML = shuffle;
+
+//function for challenge sentences to appear
 function challenge (){
-    document.getElementById('challenge-word').shuffle;
-}
+    
+};
 
-
-
-//time triggers
-let startTime = new Date();
-let endTime = new Date();
-let timeTaken = endTime - startTime;
-
-//event trigger
-function keyPressed (event) {
-    startTime = document.getElementById('type-box');
-    startTime.innerHTML = event.key;
-}
-
-//validate spelling
