2e0e4f3b942136b48538de10a7b1b6e86678af06
Musaddek Ali <mussali.one@gmail.com>
2024-01-01 15:40:19 +0000

    adjust size of challenge word

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,6 +1,7 @@
 //challenge sentences
 let sentence = [
-    "love you to the moon and back","Don't count your chickens before they hatch","A bird in the hand is worth two in the bush","Slow and steady wins the race","stop beating around the bush","the ball is in your court","You can't judge a book by its cover"
+    "Love you to the moon and back","Don't count your chickens before they hatch","A bird in the hand is worth two in the bush","Slow and steady wins the race",
+    "Stop beating around the bush","The ball is in your court","You can't judge a book by its cover"
 ];
 
 //randomise sentences
@@ -8,9 +9,4 @@ let shuffle = sentence[Math.floor(Math.random() * sentence.length)];
 
 let challengeWord = document.getElementById('challenge-word');
 challengeWord.innerHTML = shuffle;
-
-//function for challenge sentences to appear
-function challenge (){
-    
-};
 
