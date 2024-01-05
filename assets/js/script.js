89bbde8164ea4507a71a8e3ba194e3294342afa5
Musaddek Ali <mussali.one@gmail.com>
2024-01-05 18:55:39 +0000

    edit comments in js

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,11 +1,11 @@
 //challenge sentences
-let sentence = [
+const sentence = [
     "Love you to the moon and back","Don't count your chickens before they hatch","A bird in the hand is worth two in the bush","Slow and steady wins the race",
     "Stop beating around the bush","The ball is in your court","You can't judge a book by its cover"
 ];
 
 //randomise sentences
-let shuffle = sentence[Math.floor(Math.random() * sentence.length)];
+const shuffle = sentence[Math.floor(Math.random() * sentence.length)];
 
 let challengeWord = document.getElementById('challenge-word');
 challengeWord.innerHTML = shuffle;
@@ -21,12 +21,12 @@ function handleSubmit(event) {
     //if the user types the challenge incorrectly
     if (userEntry !== shuffle) {
       let errorDiv = document.getElementById('errors');
-      errorDiv.innerHTML = "<p>Uh oh...your entry did not match the challenge. Play again! but make sure you have typed in correctly.</p>";
+      errorDiv.innerHTML = "<p>Uh oh...your entry did not match the challenge. Play again! but make sure you have spely everything correctly.</p>";
       errorDiv.style.display = 'block';
       //if the challenge is correctly typed
     } else {
       let successDiv = document.getElementById('success');
-      successDiv.innerHTML = `<p>Yay! You have typed "${shuffle}" in 'Time'!</p>`;
+      successDiv.innerHTML = `<p>Yay! You have typed "${shuffle}" correctly!</p>`;
       successDiv.style.display = 'block';
     }
     userInput.disabled = true; //disable input field afer submission
@@ -38,13 +38,13 @@ function handleSubmit(event) {
 //Stopwatch
 
 // Code for the timer has been sourced from www.educative.io - edited for use on this project
-let startTime; // to keep track of the start time
-let stopwatchInterval; // to keep track of the interval
-let elapsedPausedTime = 0; // to keep track of the elapsed time while stopped
+let startTime;
+let stopwatchInterval;
+let elapsedPausedTime = 0;
 
 function startStopwatch() {
   if (!stopwatchInterval) {
-    startTime = new Date().getTime() - elapsedPausedTime; // get the starting time by subtracting the elapsed paused time from the current time
+    startTime = new Date().getTime() - elapsedPausedTime;
     stopwatchInterval = setInterval(updateStopwatch, 1); // update every second
   }
 }
@@ -61,18 +61,19 @@ function resetStopwatch() {
   document.getElementById("stopwatch").innerHTML = "00:00:00"; // reset the display
 }
 
+//function calculates time
 function updateStopwatch() {
-  let currentTime = new Date().getTime(); // get current time in milliseconds
-  let elapsedTime = currentTime - startTime; // calculate elapsed time in milliseconds
-  let milliseconds = Math.floor(elapsedTime / 1) % 60; // calculate seconds
-  let seconds = Math.floor(elapsedTime / 1000) % 60; // calculate seconds
-  let minutes = Math.floor(elapsedTime / 1000 / 60) % 60; // calculate minutes
-  let displayTime = pad(minutes) + ":" + pad(seconds) + ":" + pad(milliseconds); // format display time
-  document.getElementById("stopwatch").innerHTML = displayTime; // update the display
+  let currentTime = new Date().getTime(); //get current time in milliseconds
+  let elapsedTime = currentTime - startTime; //calculate elapsed time in milliseconds
+  let milliseconds = Math.floor(elapsedTime / 1) % 60;
+  let seconds = Math.floor(elapsedTime / 1000) % 60;
+  let minutes = Math.floor(elapsedTime / 1000 / 60) % 60;
+  let displayTime = pad(minutes) + ":" + pad(seconds) + ":" + pad(milliseconds); //format display time
+  document.getElementById("stopwatch").innerHTML = displayTime; //update the display
 }
 
 function pad(number) {
-  // add a leading zero if the number is less than 10
+  //add a leading zero if the number is less than 10
   return (number < 10 ? "0" : "") + number;
 }
 
@@ -80,3 +81,6 @@ function pad(number) {
 userInput.addEventListener('keypress', startStopwatch);
 //Timer stop tiggers when user submits
 form.addEventListener('submit', stopStopwatch);
+
+
+//Replay game
